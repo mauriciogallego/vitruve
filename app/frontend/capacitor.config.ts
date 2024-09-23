93bbc769af35dc5c17aba8720d5fef47c927f773
mauricio gallego <mgallego@sysgarage.com>
2024-09-23 16:51:18 +0000

    first commit

---
+++ app/frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontend',
+  webDir: '../../dist/app/frontend',
+  bundledWebRuntime: false,
+};
+
+export default config;
